4a6482601204ffa78ab424eaeab93c2a78ebd69c
slemi98 <drdee321@gmail.com>
2017-01-19 09:11:57 +0100

    another one

--- registration.php
+++ registration.php
@@ -11,7 +11,7 @@ include_once 'session.php';
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Konnect A Hosting Category Flat Bootstarp Resposive Website Template | Home :: w3layouts</title>
+<title>Postavi.si</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Konnect Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
